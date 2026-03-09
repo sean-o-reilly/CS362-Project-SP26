0b2f430abc4d17a964c4c8657b39802c4dcc4536
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-03-09 15:20:25 -0500

    added global variables for button pins and lcd, call setup functions for buttons and lcd in setup()

--- A3/A3.ino
+++ A3/A3.ino
@@ -1,9 +1,30 @@
+#include <LiquidCrystal.h>
+
 #include "ProgCar.h"
+
+// Pushbutton setup
+const int execute_button_pin = 13;
+const int forward_button_pin = 6;
+const int backward_button_pin = 7;
+const int left_button_pin = 8;
+const int right_button_pin = 9;
+const int speed_button_pin = 10;
+
+// LCD setup
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 using namespace pgc;
 
 void setup() {
   initSlave(A3_ADDRESS);
+  
+  // Pushbutton setup
+  pinMode(left_button_pin, INPUT_PULLUP);
+  pinMode(right_button_pin, INPUT_PULLUP);
+
+  // LCD setup
+  lcd.begin(16, 2);
 }
 
 void getMove() {
