08f55a8cdec604f4dba75510dcb5edbf735de0f9
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-03-12 12:15:38 -0500

    started A3 class with Button subclass

--- A3/A3.ino
+++ A3/A3.ino
@@ -1,6 +1,70 @@
+#include <LiquidCrystal.h>
+
 #include "ProgCar.h"
 
+#define DEBOUNCE_DELAY 50
+
 using namespace pgc;
+
+class A3 {
+
+  class Button {
+
+    private:
+      const unsigned int pinNumber;
+      unsigned int currState, prevState;
+      unsigned long lastDebounce;
+
+    public:
+
+      Button(int pinNumber) {
+        this->pinNumber = pinNumber;
+        this->currState = LOW;
+        this->prevState = LOW;
+        this->lastDebounce = 0;
+        pinMode(pin, INPUT_PULLUP);
+      }
+
+      /*
+      Returns 1 if button is pressed, else returns 0.
+      */
+      int getInput() {
+        int reading = digitalRead(this->pinNumber);
+
+        if (reading != this->prevState) {
+          lastDebounce = millis();
+        }
+
+        if ((millis() - this->lastDebounce) > DEBOUNCE_DELAY) {
+          if (reading != this->currState) {
+            this->currState = reading;
+            if (this->currState == HIGH) {
+              return 1;
+            }
+          }
+        }
+
+        last_left_button_state = reading;
+        return 0;
+      }
+
+  }
+
+  private:
+    Button execute, forward, backward, left, right, speed;
+    LiquidCrystal lcd;
+
+  public:
+    A3(int execute_pin, 
+    int forward_pin, 
+    int backward_pin, 
+    int left_pin, 
+    int right_pin,
+    int speed_pin) {
+      
+    }
+
+};
 
 void setup() {
   initSlave(A3_ADDRESS);
