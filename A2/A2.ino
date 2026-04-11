d0e5b2b02e03a78b8f43af72bf44696d6e86a7d3
dormantgoodness <aiddom107@gmail.com>
2026-04-10 23:12:04 -0500

    Steering arduino

--- A2/A2.ino
+++ A2/A2.ino
@@ -1,7 +1,62 @@
 #include "ProgCar.h"
+#include <Servo.h> 
+
+using namespace pgc;
+
+//  Hardware Setup
+Servo steeringServo;
+const int SERVO_PIN = 3; 
+// Steering Angles
+const int ANGLE_LEFT = 45;
+const int ANGLE_STRAIGHT = 90;
+const int ANGLE_RIGHT = 135;
+
+//  Execution Function
+void executeSteering() {
+  SteerDir currentDir = SteerDir::STRAIGHT; 
+
+  // Read the payload sent by the Master
+  if (Wire.available()) {
+    currentDir = static_cast<SteerDir>(Wire.read());
+  }
+
+  // Command the servo (this triggers the hardware timer, no blocking required)
+  switch (currentDir) {
+    case SteerDir::LEFT:
+      steeringServo.write(ANGLE_LEFT);
+      Serial.println("Action: Commanded LEFT");
+      break;
+    case SteerDir::RIGHT:
+      steeringServo.write(ANGLE_RIGHT);
+      Serial.println("Action: Commanded RIGHT");
+      break;
+    case SteerDir::STRAIGHT:
+    default:
+      steeringServo.write(ANGLE_STRAIGHT);
+      Serial.println("Action: Commanded STRAIGHT");
+      break;
+  }
+
+  // Immediately report success. 
+  // The servo will physically travel to its position in the background
+  // while the Master is freed up to immediately trigger the Drive Arduino (A1).
+  reportToMaster(WORK_DONE);
+}
 
 void setup() {
+  Serial.begin(BAUD_RATE);
+  
+  // Initialize this Arduino as Slave A2
+  initSlave(A2_ADDRESS);
+
+  // Attach the servo and set it to straight by default
+  steeringServo.attach(SERVO_PIN);
+  steeringServo.write(ANGLE_STRAIGHT);
+  
+  Serial.println("A2 Steering Arduino Initialized (Non-Blocking).");
 }
 
 void loop() {
+  // Checks for commands and runs executeSteering instantly when triggered
+  runSlaveCommand(executeSteering);
 }
