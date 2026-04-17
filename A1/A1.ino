148f8afd38a23d8dfa5b8a6cc263895cd6534f93
Sean OReilly <oreilly.e.sean@gmail.com>
2026-04-17 15:56:51 -0500

    fix: floating point bug

--- A1/A1.ino
+++ A1/A1.ino
@@ -5,16 +5,16 @@ using namespace pgc;
 
 // Stepper configuration
 const int STEPS_PER_REV = 2048;
-Stepper driveMotor(STEPS_PER_REV, 2, 4, 3, 5); // IN1, IN3, IN2, IN4
+Stepper driveMotor(STEPS_PER_REV, 2, 4, 3, 5);  // IN1, IN3, IN2, IN4
 
 // Sensor Pins
 const int TRIG_PIN = 6;
 const int ECHO_PIN = 7;
 const int BUZZER_PIN = 10;
-const int OBSTACLE_THRESHOLD = 15; 
+const int OBSTACLE_THRESHOLD = 15;
 
 void setup() {
-  initSlave(A1_ADDRESS); /
+  initSlave(A1_ADDRESS);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
@@ -28,10 +28,10 @@ bool isPathBlocked() {
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-  
+
   long duration = pulseIn(ECHO_PIN, HIGH);
-  const int SOUND_SPEED = 0.034
-   int distance = duration * SOUND_SPEED / 2;
+  constexpr float soundSpeed = 0.034;
+  int distance = duration * soundSpeed / 2;
 
   if (distance > 0 && distance < OBSTACLE_THRESHOLD) {
     digitalWrite(BUZZER_PIN, HIGH);
@@ -43,33 +43,33 @@ bool isPathBlocked() {
 
 void runDriveCommand(Gas gas, Speed speed) {
   int motorSpeed = 0;
-  int travelSteps = 1000; // distance per move
+  int travelSteps = 1000;  // distance per move
 
   // Set speed based on the Speed enum
   if (speed == Speed::LOW_SPEED) {
-      motorSpeed = 5;
+    motorSpeed = 5;
   } else if (speed == Speed::MEDIUM_SPEED) {
-      motorSpeed = 10;
+    motorSpeed = 10;
   } else if (speed == Speed::HIGH_SPEED) {
-      motorSpeed = 15;
+    motorSpeed = 15;
   }
-  
+
   driveMotor.setSpeed(motorSpeed);
   int direction;
   if (gas == Gas::FORWARD) {
-      direction = 1;
+    direction = 1;
   } else {
-      direction = -1;
+    direction = -1;
   }
 
   // check sensor frequently
-  const int STEPS = 10
-  for (int i = 0; i < travelSteps; i += STEPS) {
+  constexpr int steps = 10;
+  for (int i = 0; i < travelSteps; i += steps) {
     if (isPathBlocked()) {
       Serial.println("Obstacle detected.");
-      break; 
+      break;
     }
-    driveMotor.step(STEPS * direction);
+    driveMotor.step(steps * direction);
   }
 
   reportToMaster(WORK_DONE);
@@ -78,7 +78,7 @@ void runDriveCommand(Gas gas, Speed speed) {
 void loop() {
   // check for commands
   handleDriveCommand(runDriveCommand);
-  
+
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   delay(100);
 }
