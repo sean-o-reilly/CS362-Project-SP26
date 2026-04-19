4dae53137df2c585df3597cb59ac14d0ea57f31e
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-04-19 13:16:54 -0500

    added custom steering speed logic in the form of a for loop that iterates the servo position up to the desired angle. each write() call occurs after a specified time interval has passed.

--- A2/A2.ino
+++ A2/A2.ino
@@ -7,6 +7,11 @@ Servo servo;
 
 constexpr int SERVO_PIN = 9;
 
+// variables for delay between each call to write()
+// for slower spinning, set timeInterval to a higher value
+unsigned long currTime, prevTime = 0;
+unsigned long timeInterval = 15;
+
 void setup() {
   initSlave(A2_ADDRESS);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -14,11 +19,24 @@ void setup() {
   Serial.begin(BAUD_RATE);
 }
 
+/*
+Spins servo motor from its current position to the desired angle at a custom speed.
+In order to reduce jitter and inconsistent spinning, there is custom delay logic
+between each call to write().
+*/
+void spinServo(int finalAngle) {
+  for (int pos = servo.read(); pos < finalAngle; pos++) {
+    if ((currTime - prevTime) > timeInterval) {
+      servo.write(pos);
+      prevTime = currTime;
+    }
+  }
+}
+
 void runSteerCommand(SteerDir steerDir) {
   int angle = static_cast<int>(steerDir);
   Serial.println(angle);
-  servo.write(angle);
-  while (servo.read() != angle);
+  spinServo(angle);
   reportToMaster(WORK_DONE);
 }
 
