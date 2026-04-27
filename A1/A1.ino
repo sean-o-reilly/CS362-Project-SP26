7990f61062f6d46b6c64095ca161ee69b8fb798f
YoussefEgh <youssefelghawabi@gmail.com>
2026-04-27 14:37:10 -0500

    Refactor motor control and sensor configuration

--- A1/A1.ino
+++ A1/A1.ino
@@ -3,15 +3,17 @@
 
 using namespace pgc;
 
-// Stepper configuration
 const int STEPS_PER_REV = 2048;
-Stepper driveMotor(STEPS_PER_REV, 2, 4, 3, 5);  // IN1, IN3, IN2, IN4
+Stepper leftMotor(STEPS_PER_REV, 2, 4, 3, 5);
+Stepper rightMotor(STEPS_PER_REV, 6, 7, 8, 9);
 
-// Sensor Pins
-const int TRIG_PIN = 6;
-const int ECHO_PIN = 7;
-const int BUZZER_PIN = 10;
+const int TRIG_PIN = 10;
+const int ECHO_PIN = 11;
+const int BUZZER_PIN = 12;
 const int OBSTACLE_THRESHOLD = 10;
+
+unsigned long lastBlink = 0;
+const int blinkInterval = 100;
 
 void setup() {
   initSlave(A1_ADDRESS);
@@ -21,14 +23,12 @@ void setup() {
   Serial.begin(BAUD_RATE);
 }
 
-// check for obstacles
 bool isPathBlocked() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-
 
   long duration = pulseIn(ECHO_PIN, HIGH);
   const float soundSpeed = 0.034;
@@ -47,9 +47,8 @@ bool isPathBlocked() {
 
 void runDriveCommand(Gas gas, Speed speed) {
   int motorSpeed = 0;
-  int travelSteps = 1000;  // distance per move
+  int travelSteps = 1000;
 
-  // Set speed based on the Speed enum
   if (speed == Speed::LOW_SPEED) {
     motorSpeed = 5;
   } else if (speed == Speed::MEDIUM_SPEED) {
@@ -58,34 +57,41 @@ void runDriveCommand(Gas gas, Speed speed) {
     motorSpeed = 15;
   }
 
-  driveMotor.setSpeed(motorSpeed);
-  int direction;
+  leftMotor.setSpeed(motorSpeed);
+  rightMotor.setSpeed(motorSpeed);
+
+  int leftDirection, rightDirection;
   if (gas == Gas::FORWARD) {
-    direction = 1;
+    leftDirection = 1;
+    rightDirection = -1;
   } else {
-    direction = -1;
+    leftDirection = -1;
+    rightDirection = 1;
   }
 
-  // check sensor frequently
   const int STEPS = 10;
   for (int i = 0; i < travelSteps; i += STEPS) {
     if (isPathBlocked()) {
       Serial.println("Obstacle detected.");
-      delay(500);
-      reportToMaster(OBSTACLE_ERROR);
+      unsigned long startWait = millis();
+      while (millis() - startWait < 500) {}
       digitalWrite(BUZZER_PIN, LOW);
+      reportToMaster(OBSTACLE_ERROR);
       return;
     }
-    driveMotor.step(STEPS * direction);
+    leftMotor.step(STEPS * leftDirection);
+    rightMotor.step(STEPS * rightDirection);
   }
 
   reportToMaster(WORK_DONE);
 }
 
 void loop() {
-  // check for commands
   handleDriveCommand(runDriveCommand);
 
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  delay(100);
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastBlink >= blinkInterval) {
+    lastBlink = currentMillis;
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
 }
